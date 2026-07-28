e3482807a9034a67b9f2fd1fcd7acf7b7109b6d7
Goodtimes26 <Goodtimes26@users.noreply.github.com>
2026-07-28 19:24:33 +0200

    Vervang foto van Esther

--- app/site.tsx
+++ app/site.tsx
@@ -114,7 +114,7 @@ function PageIntro({ kicker, title, accent, text, className = "" }: { kicker: st
 
 function About() {
   const members = [
-    ["ESTHER", "Zang", "/members/esther-zang.png", "Esther, zangeres van GoodTimes"],
+    ["ESTHER", "Zang", "/members/esther-zang.jpeg", "Esther, zangeres van GoodTimes"],
     ["CINDY", "Zang", "/members/cindy-zang.png", "Cindy, zangeres van GoodTimes"],
     ["LUUK", "Toetsen", "/members/luuk-toetsen.jpg", "Luuk achter de toetsen bij GoodTimes"],
     ["JOOST", "Gitaar", "/members/joost-gitaar.jpg", "Joost speelt gitaar bij GoodTimes"],
